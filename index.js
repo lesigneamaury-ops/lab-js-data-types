46dcd4838f90234e0679351ef5335fa7b485a9ba
Amaury Lesigne <lesigne.amaury@gmail.com>
2025-11-19 14:43:14 +0100

    Lab1Day3

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
-
+const mixedVariables = `${s1} ${s2} ${s3} ${s4} ${s5}`
+const mixedVariables2 = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5
 // Print out the concatenated string
-
+console.log(mixedVariables);
+console.log(mixedVariables2);
 
 
 
@@ -22,10 +23,11 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+const upperPart1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase(); 
+const upperPart2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+const lastUpperPart = upperPart1 + upperPart2
 // Print the cameLtaiL-formatted string
-
+console.log(lastUpperPart);
 
 
 
@@ -35,10 +37,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const billPlusTip = billTotal + (billTotal * 0.15)
 
 // Print out the tipAmount
-
+console.log(billPlusTip);
 
 
 
@@ -47,10 +49,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random()*10 +1);
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +64,16 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+//false
 const expression2 = a || b;
-
+//true
 const expression3 = !a && b;
-
+//true
 const expression4 = !(a && b);
-
+//true
 const expression5 = !a || !b;
-
+//true
 const expression6 = !(a || b);
-
+//false
 const expression7 = a && a;
+//true
